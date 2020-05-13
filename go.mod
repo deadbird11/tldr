e634d3054741f92a9af4ed80eea71635d3055618
deadbird11 <eddiejr@hatfieldfamily.com>
2020-05-12 22:25:17 -0400

    beginning implementing renderer with color library, via https://github.com/fatih/color

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/deadbird11/tldr
 
 go 1.14
+
+require github.com/fatih/color v1.9.0
---
+++ render/render.go
@@ -0,0 +1,7 @@
+package render
+
+// RenderMD - takes a string written in github markdown
+// and prints it out in a prettier way, with colors
+func RenderMD(content string) string {
+
+}
