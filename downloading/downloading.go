c19400825426fe02e8f0e64a21bc1ade8b83f37a
deadbird11 <eddiejr@hatfieldfamily.com>
2020-05-23 12:32:45 -0400

    added support for commands from all os', fixed 404 page issue

--- downloading/downloading.go
+++ downloading/downloading.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"runtime"
+	"strings"
 )
 
 const (
@@ -13,9 +15,12 @@ const (
 	cacheExtension = ".txt"
 
 	remoteURLBase   = "https://raw.githubusercontent.com/tldr-pages/tldr/master/pages/"
-	remoteDir       = "common/"
 	remoteExtension = ".md"
 )
+
+func swap(arr []string, idx1 int, idx2 int) {
+	arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
+}
 
 // GetCommandDesc - takes a command "cmd" and returns the tldr
 // description of it, via https://github.com/tldr-pages/tldr/tree/master/pages
@@ -25,8 +30,24 @@ func GetCommandDesc(cmd string) *string {
 		return desc
 	}
 
-	if desc := getRemoteDesc(cmd); desc != nil {
-		return desc
+	remoteDirs := []string{"windows/", "common/", "linux/", "osx/", "sunos/"}
+
+	switch runtime.GOOS {
+	case "windows":
+		break
+	case "linux":
+		swap(remoteDirs, 0, 2)
+		break
+	default:
+		swap(remoteDirs, 0, 3)
+		break
+	}
+	for _, dir := range remoteDirs {
+		fmt.Printf("searching directory %s\n", dir)
+		if desc := getRemoteDesc(cmd, dir); desc != nil {
+			fmt.Printf("'%s' command detected...downloading description\n", dir[:len(dir)-1])
+			return desc
+		}
 	}
 	return nil
 }
@@ -46,8 +67,8 @@ func getCachedDesc(cmd string) *string {
 // from https://github.com/tldr-pages/tldr/tree/master/pages,
 // TODO: add support for specific OSs, starting with the one the user is
 // currently using
-func getRemoteDesc(cmd string) *string {
-	desc, err := tryDownload(remoteURLBase + remoteDir + cmd + remoteExtension)
+func getRemoteDesc(cmd string, dir string) *string {
+	desc, err := tryDownload(remoteURLBase + dir + cmd + remoteExtension)
 	if err != nil {
 		return nil
 	}
@@ -55,6 +76,10 @@ func getRemoteDesc(cmd string) *string {
 	err = tryWriteToFile(desc, cmd)
 	if err != nil {
 		fmt.Println("failed to cache description")
+	}
+
+	if strings.Contains(*desc, "404") {
+		return nil
 	}
 
 	return desc
